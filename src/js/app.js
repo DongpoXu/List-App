d990b82dc7e081df5cabf050043410d7a1f3032b
Aelousdp <806267856@qq.com>
2018-12-15 21:55:44 +0800

    添加删除，完成

--- src/js/app.js
+++ src/js/app.js
@@ -3,24 +3,34 @@
  * @date 2018-12-13
  * @author XDP
  */
-/*
 // 清除之前项目的本地存储数据
-localStorage.removeItem("List_todo");
+localStorage.removeItem("LIST_TODO");
+localStorage.removeItem("LIST_COMPLETED");
+
 let model = Model(),
     render = Render();
 
 // 入口
 render.init();
 
-// 默认显示todo列表页面
-$("#todoList").show();
-*/
+// 获取DOM
+let addBtn = $("#addBtn"),
+    addContent = $("#addContent"),
+    okAddBtn = $("#okAddBtn"),
+    inputValue = $("#inputValue");
 
+let todoList = $("#todoList");
+let completedList = $("#completedList");
+
+// 默认显示todo列表页面
+todoList.show();
+
+/**
+ * @desc 添加任务按钮模块
+ * @date 2018-12-15
+ * @author XDP
+ */
 (function () {
-    let addBtn = $("#addBtn"),
-        addContent = $("#addContent"),
-        okAddBtn = $("#okAddBtn"),
-        inputValue = $("#inputValue");
     // 添加新的任务
     addBtn.click(function () {
         if (addContent.css("display") === "none") {       // 判断添加任务页面是否存在
@@ -50,17 +60,19 @@ $("#todoList").show();
         let text = inputValue.val();
         if (text) {
             model.addTask(text);
-            inputValue.value = "";
-            addContent.style.top = addContent.style.left = "100%";
-            okAddBtn.style.bottom = "0px";
-            okAddBtn.css("bottom", "5px");
+            inputValue.val("");
+            addContent.fadeOut("100");
+            addBtn.css({
+                'transform': 'rotate(0deg)',
+                '-webkit-transform': 'rotate(0deg)'
+            });
+            okAddBtn.css("bottom", "0px");
             okAddBtn.css("background-color", "#757575");
             inputValue.css("border-bottom-color", "#BDBDBD");
-
-            // TODO:切换到todo列表
-            completedList.style.display = "none";
-            todoList.style.display = "block";
-
+            // TODO:关闭侧边栏和遮罩层
+            // 切换到todo列表
+            $('#completedList').fadeOut();
+            $('#todoList').fadeIn();
         }
     });
 
@@ -69,7 +81,7 @@ $("#todoList").show();
         let vInput = inputValue.val();
         // 如果有输入则控件变色
         if (vInput !== "") {
-            okAddBtn.css("background-color", "#238cee");
+            okAddBtn.css("background-color", appColor().mainColor);
             inputValue.css("border-bottom-color", "#212121");
         }
         // 如果输入为空则变为默认色
@@ -80,109 +92,90 @@ $("#todoList").show();
     });
 })();
 
-/*
-
-/!**
+/**
  * @desc 清除/完成操作
  * @date 2018-12-13
  * @author XDP
- *!/
-// todoList.getElementsByClassName("clearTask")[0].onclick = function () {
-//     model.clearTask('List_todo', this.parentNode.getElementsByTagName('i')[0].textContent);
-// }
-// todoList.getElementsByClassName("finishTask")[0].onclick = function () {
-//     model.completeTask(this.parentNode.textContent);
-//     model.clearTask('List_todo', this.parentNode.getElementsByTagName('i')[0].textContent);
-// }
-// completedList.getElementsByClassName("clearTask")[0].onclick = function () {
-//     model.clearTask('List_completed', this.parentNode.getElementsByTagName('i')[0].textContent);
-// }
-
-$("#todoList").on("click", ".clear", function () {
-    console.log($(this).parent().find('i').text());
-    model.clearTask('List_todo', $(this).parent().find('i').text());
-});
-$("#todoList").on("click", ".finish", function () {
-    //先添加到已完成列表，然后在当前删除
-    model.completeTask($(this).parent().find('.task-title').text());
-    model.clearTask('List_todo', $(this).parent('.task-item').find('i').text());
-});
-$("#completedList").on("click", ".clear", function () {
-    model.clearTask('List_completed', $(this).parent().find('i').text());
+ */
+todoList.on("click", ".clear-task", function () {
+    model.clearTask('LIST_TODO', $(this).parent().find('i').text);
 });
 
-/!**
+todoList.on("click", ".finish-task", function () {
+    model.completeTask($(this).parent().find(".task-title").text());
+    model.clearTask('LIST_TODO', $(this).parent(".task-item").find('i').text);
+});
+
+completedList.on("click", ".clear-task", function () {
+    model.clearTask('LIST_COMPLETE', $(this).parent().find('i').text);
+});
+
+/**
  * @desc 数据存储---本地
  * @date 2018-12-13
  * @author XDP
- *!/
+ */
 function Model() {
-    let addTask = function (add) {      // 添加
-        let taskItem = [localStorage["List_todo"]];
-        taskItem.push(add);
-        localStorage.setItem("List_todo", taskItem);
+    let addTask = function (addItem) {      // 添加
+        let taskItem = [localStorage["LIST_TODO"]];
+        taskItem.push(addItem);
+        localStorage.setItem("LIST_TODO", taskItem);
         render.init();
     };
-    let completeTask = function (complete) {        //完成
-        let taskItem = [localStorage["List_completed"]];
-        taskItem.push(complete);
-        localStorage.setItem("List_completed", taskItem);
-        var newArr = localStorage['List_completed'].split(',');
-        let ele = document.getElementById("completedList");
-        ele.innerHTML = "";
-        let taskDom = parseDom(
+    let completeTask = function (completeItem) {        // 完成
+        let taskItem = [localStorage["LIST_COMPLETED"]];
+        taskItem.push(completeItem);
+        localStorage.setItem("LIST_COMPLETED", taskItem);
+        let newArr = localStorage['LIST_COMPLETED'].split(',');
+        $("#completedList").prepend(
             "<li class='task-item'>"
-            + "<span class='task-title'>" + complete + "</span>"
+            + "<span class='task-title'>" + completeItem + "</span>"
             + "<i>" + (newArr.length - 1) + "</i>"
-            + "<div class='clearTask fa fa-trash-o'>" + "</div>"
-            + "<div class='finishTask fa fa-check-circle-o'>" + "</div>"
+            + "<div class='clear-task fa fa-trash-o'>" + "</div>"
+            + "<div class='finish-task fa fa-check-circle-o'>" + "</div>"
             + "</li>");
-        // 注意，insertBefore必须插入节点，不能是字符串
-        ele.insertBefore(taskDom[0], ele.childNodes[0]);
+        // 注意，如果使用原生JS，insertBefore必须插入节点，不能是字符串
     };
-    var clearTask = function (list, i) {
-        var arr, listName, el;
-        if (list == "List_todo") {
-            listName = "List_todo";
+    let clearTask = function (listItem, i) {        // 删除
+        let arr,        // 缓存数组
+            listName,   // 本地缓存对象
+            el;         // DOM对象
+        if (listItem === "LIST_TODO") {
+            listName = "LIST_TODO";
             el = "todoList";
-        } else if (list == "List_completed") {
-            listName = "List_completed";
+        } else if (listItem === "LIST_COMPLETED") {
+            listName = "LIST_COMPLETED";
             el = "completedList";
         }
         arr = localStorage[listName].split(',');
         arr.splice(i, 1);
         localStorage.setItem(listName, arr);
         render.init(el);
-    }
+    };
     return {
         addTask: addTask,
         completeTask: completeTask,
         clearTask: clearTask
     };
-    return {
-        addTask: addTask,
-        completeTask: completeTask,
-        // clearTask: clearTask
-    };
 }
 
-/!**
+/**
  * @desc 渲染器---任务列表
  * @date 2018-12-13
  * @author XDP
- *!/
+ */
 function Render() {
     let tasks,
         taskTitle;
 
     let init = function (e) {
-        var el = e || "todoList",
+        let el = e || "todoList",
             listName;
 
         if (el === "todoList") {
-            listName = "List_todo";
+            listName = "LIST_TODO";
         } else if (el === "completedList") {
-            listName = "List_completed";
+            listName = "LIST_COMPLETED";
         }
 
         let ele = document.getElementById(el);
@@ -196,8 +189,8 @@ function Render() {
                     "<li class='task-item'>"
                     + "<span class='task-title'>" + taskTitle + "</span>"
                     + "<i>" + i + "</i>"
-                    + "<div class='clearTask fa fa-trash-o'>" + "</div>"
-                    + "<div class='finishTask fa fa-check-circle-o'>" + "</div>"
+                    + "<div class='clear-task fa fa-trash-o'>" + "</div>"
+                    + "<div class='finish-task fa fa-check-circle-o'>" + "</div>"
                     + "</li>");
                 // 注意，insertBefore必须插入节点，不能是字符串
                 ele.insertBefore(taskDom[0], ele.childNodes[0]);
@@ -209,15 +202,28 @@ function Render() {
     };
 }
 
-/!**
+/**
  * @desc 字符串转换为DOM节点
  * @date 2018-12-13
  * @author XDP
- *!/
+ */
 function parseDom(str) {
     let objE = document.createElement("div");
     objE.innerHTML = str;
     return objE.childNodes;
 }
 
-*/
+/**
+ * @desc 换肤功能
+ * @date 2018-12-15
+ * @author XDP
+ */
+function appColor() {
+    let mainColor = localStorage.APP_COLOR || "#238cee",
+        secColor = "#FFC107";
+    $('#header').css('background-color', mainColor);
+    $('#addBtn').css('background-color', secColor);
+    return {
+        mainColor: mainColor
+    };
+}
